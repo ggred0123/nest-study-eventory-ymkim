327ef9f8c0f822d727c44dc5fa9f2e33a1d4a006
ggred0123 <ggred0123@gmail.com>
2024-11-18 23:16:48 +0900

    진짜 구현 시작클럽 생성 api 구현

---
+++ src/club/club.repository.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../common/services/prisma.service';
+import { CreateClubData } from './type/create-club-data.type';
+import { ClubData } from './type/club-data.type';
+import { User, Club, ClubJoin, WaitingStatus } from '@prisma/client';
+import { EventData } from 'src/event/type/event-data.type';
+@Injectable()
+export class ClubRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createClub(data: CreateClubData): Promise<ClubData> {
+    return this.prisma.club.create({
+      data: {
+        leadId: data.leadId,
+        name: data.name,
+        description: data.description,
+        maxPeople: data.maxPeople,
+        clubJoin: {
+          create: {
+            userId: data.leadId,
+          },
+        },
+      },
+      select: {
+        id: true,
+        leadId: true,
+        name: true,
+        description: true,
+        maxPeople: true,
+      },
+    });
+  }
+}
