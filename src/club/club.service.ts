b2c21a0d73564fc17265d49ce527b06bf92cdc0c
ggred0123 <ggred0123@gmail.com>
2024-11-19 00:22:29 +0900

    클럽 조인 승인 api 구현

--- src/club/club.service.ts
+++ src/club/club.service.ts
@@ -49,4 +49,45 @@ export class ClubService {
 
     await this.clubRepository.joinClubWaiting(clubId, user.id);
   }
+
+  async approveClubJoin(
+    clubId: number,
+    userId: number,
+    approve: boolean,
+    user: UserBaseInfo,
+  ): Promise<void> {
+    const club = await this.clubRepository.getClubById(clubId);
+
+    if (!club) {
+      throw new NotFoundException('Club가 존재하지 않습니다.');
+    }
+
+    const IsUserWaitingClub = await this.clubRepository.isUserWaitingClub(
+      clubId,
+      userId,
+    );
+    if (!IsUserWaitingClub) {
+      throw new ConflictException('해당 유저가 대기중인 클럽이 아닙니다.');
+    }
+
+    await this.checkLeadPermissionOfClub(clubId, user.id);
+
+    if (!approve) {
+      await this.clubRepository.rejectClubJoin(clubId, userId);
+    } else {
+      await this.clubRepository.approveClubJoin(clubId, userId);
+    }
+  }
+
+  private async checkLeadPermissionOfClub(clubId: number, userId: number) {
+    const club = await this.clubRepository.getClubById(clubId);
+
+    if (!club) {
+      throw new NotFoundException('club가 존재하지 않습니다.');
+    }
+
+    if (club.leadId !== userId) {
+      throw new ForbiddenException('리드가 아닙니다!');
+    }
+  }
 }
