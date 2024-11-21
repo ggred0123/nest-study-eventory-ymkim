c02f7a803a4db2351fabd39c0fa40c1e535263de
ggred0123 <ggred0123@gmail.com>
2024-11-21 15:29:11 +0900

    클럽 조인 승인 api 구현 #23

--- src/club/club.controller.ts
+++ src/club/club.controller.ts
@@ -64,7 +64,6 @@ export class ClubController {
   @ApiNoContentResponse()
   async decideClubJoin(
     @Param('clubId', ParseIntPipe) clubId: number,
-    @Param('userId', ParseIntPipe) userId: number,
     @Body() payload: ApproveClubJoinPayload,
     @CurrentUser() user: UserBaseInfo,
   ): Promise<void> {
