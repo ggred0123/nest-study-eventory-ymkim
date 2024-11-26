51e6da18fe8c1318348b1f19496d12cff8a22cde
ggred0123 <ggred0123@gmail.com>
2024-11-26 11:19:10 +0900

    클럽 내 이벤트에 관한 리뷰 생성, 조회, 수정, 삭제 변경

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -14,6 +14,7 @@ import { PutUpdateReviewPayload } from './payload/put-update-review.payload';
 import { PatchUpdateReviewPayload } from './payload/patch-update-review.payload';
 import { UserBaseInfo } from '../auth/type/user-base-info.type';
 import { ReviewData } from './type/review-data.type';
+import { filter } from 'lodash';
 
 @Injectable()
 export class ReviewService {
@@ -43,6 +44,15 @@ export class ReviewService {
     if (!event) {
       throw new NotFoundException('Event가 존재하지 않습니다.');
     }
+    if (event.clubId) {
+      const userInClub = await this.reviewRepository.isUserJoinedClub(
+        user.id,
+        event.clubId,
+      );
+      if (!userInClub) {
+        throw new ConflictException('해당 유저가 클럽에 가입하지 않았습니다.');
+      }
+    }
 
     if (event.endTime > new Date()) {
       throw new ConflictException(
@@ -69,20 +79,69 @@ export class ReviewService {
     return ReviewDto.from(review);
   }
 
-  async getReviewById(reviewId: number): Promise<ReviewDto> {
+  async getReviewById(
+    reviewId: number,
+    user: UserBaseInfo,
+  ): Promise<ReviewDto> {
     const review = await this.reviewRepository.getReviewById(reviewId);
 
     if (!review) {
       throw new NotFoundException('Review가 존재하지 않습니다.');
     }
+    const event = await this.reviewRepository.getEventById(review.eventId);
+    if (!event) {
+      throw new NotFoundException('Event가 존재하지 않습니다.');
+    }
+    if (event.clubId) {
+      const userInClub = await this.reviewRepository.isUserJoinedClub(
+        user.id,
+        event.clubId,
+      );
+      if (!userInClub) {
+        throw new ConflictException('해당 유저가 클럽에 가입하지 않았습니다.');
+      }
+    }
 
     return ReviewDto.from(review);
   }
+  //
 
-  async getReviews(query: ReviewQuery): Promise<ReviewListDto> {
+  async getReviews(
+    query: ReviewQuery,
+    user: UserBaseInfo,
+  ): Promise<ReviewListDto> {
     const reviews = await this.reviewRepository.getReviews(query);
+    const filteredReviews = await this.filterEventReviewInUserJoinedClub(
+      reviews,
+      user,
+    );
 
-    return ReviewListDto.from(reviews);
+    return ReviewListDto.from(filteredReviews);
+  } // 내가 하고자 하는거.. 이게 보면 리뷰들 중에서도 클럽 안이벤트가 있고 그렇지 않은게 있을거야냐..
+  //리뷰를 적은 이벤트가 클럽 안에서 만들어진거면 지금 조회를 하고 있는 유저가 그 클럽 안에 속한 사람인지 확인
+
+  async filterEventReviewInUserJoinedClub(
+    reviews: ReviewData[],
+    user: UserBaseInfo,
+  ) {
+    const filteredReviews = reviews.filter(async (review) => {
+      const event = await this.reviewRepository.getEventById(review.eventId);
+      if (!event) {
+        throw new NotFoundException('Event가 존재하지 않습니다.');
+      }
+      if (event.clubId) {
+        const userInClub = await this.reviewRepository.isUserJoinedClub(
+          user.id,
+          event.clubId,
+        );
+        if (!userInClub) {
+          return false;
+        }
+      }
+      return true;
+    });
+
+    return filteredReviews;
   }
 
   async putUpdateReview(
@@ -149,6 +208,20 @@ export class ReviewService {
 
     if (!review) {
       throw new NotFoundException('Review가 존재하지 않습니다.');
+    }
+
+    const event = await this.reviewRepository.getEventById(review.eventId);
+    if (!event) {
+      throw new NotFoundException('Event가 존재하지 않습니다.');
+    }
+    if (event.clubId) {
+      const userInClub = await this.reviewRepository.isUserJoinedClub(
+        userId,
+        event.clubId,
+      );
+      if (!userInClub) {
+        throw new ConflictException('해당 유저가 클럽에 가입하지 않았습니다.');
+      }
     }
 
     if (review.userId !== userId) {
